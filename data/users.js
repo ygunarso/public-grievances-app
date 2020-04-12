a8dc7bc954117714b3e93452b9c35f911e4231b4
samirshah120 <56420676+samirshah120@users.noreply.github.com>
2020-04-12 18:40:10 -0400

    add and fetch user

--- data/users.js
+++ data/users.js
@@ -1,7 +1,39 @@
 const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
+const issues = mongoCollections.issues;
 const { ObjectId } = require('mongodb');
 
-// TODO
+// const { ObjectId } = require("mongodb").ObjectID;
 
-module.exports = {  };
+// TODO
+let exportedMethods = {
+    async getAllUsers() {
+      const userCollection = await users();
+      const userList = await userCollection.find({}).toArray();
+      if (!userList) throw 'No user in system!';
+      return userList
+    },
+    async getUserById(id) {
+      id = ObjectId(id)
+      const userCollection = await users();
+      const user = await userCollection.findOne({_id: id});
+      return user
+    },
+    async addUser(firstName,lastName,email,city,state) {
+      const userCollection = await users();
+      let newUser = {
+        firstName: firstName,
+        lastName : lastName,
+        email : email,
+        city : city,
+        // hashedPassword : hashedPassword,
+        issues: [],
+        state : state 
+      };
+  
+      const newInsertInformation = await userCollection.insertOne(newUser);
+      if (newInsertInformation.insertedCount === 0) throw 'Insert failed!';
+      return await this.getUserById(newInsertInformation.insertedId);
+    }
+   };
+module.exports = exportedMethods;
--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,61 @@ const data = require('../data');
 const usersData = data.users;
 const { ObjectId } = require('mongodb');
 
+// router.get('/', async (req, res) => {
+//     res.render('grievances/index');
+// });
 
+router.get('/:id', async (req, res) => {
+	try {
+		let user = await usersData.getUserById(req.params.id);
+		if(user === null || user === undefined)
+			res.status(404).json({ error: 'user not found' })
+		else
+			res.json(user);
+	} catch (e) {
+		res.status(404).json({ error: 'user not found' });
+	}
+});
 router.get('/', async (req, res) => {
-    res.render('grievances/index');
+	try {
+		let userList = await usersData.getAllUsers();
+		res.json(userList);
+	} catch (e) {
+		res.sendStatus(400);
+	}
+});
+router.post('/', async (req, res) => {
+	let userInfo = req.body;
+	if (!userInfo) {
+		res.status(400).json({ error: 'You must provide data to create a user' });
+		return;
+	}
+	if (!userInfo.firstName) {
+		res.status(400).json({ error: 'You must provide a first name' });
+		return;
+	}
+	if (!userInfo.lastName) {
+		res.status(400).json({ error: 'You must provide a last name' });
+		return;
+	}
+	if (!userInfo.email) {
+		res.status(400).json({ error: 'You must provide an email' });
+		return;
+	}
+	if (!userInfo.city) {
+		res.status(400).json({ error: 'You must provide city' });
+		return;
+	}
+	if (!userInfo.state) {
+			res.status(400).json({ error: 'You must provide state' });
+			return;
+	}
+	try {
+		const newUser = await usersData.addUser(userInfo.firstName,userInfo.lastName,userInfo.email,userInfo.city,userInfo.state);
+		res.json(newUser);
+	} catch (e) {
+		res.sendStatus(400);
+	}
 });
 
 module.exports = router;
