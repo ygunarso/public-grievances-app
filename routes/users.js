6b0a20ff312bf258138d98651862bf7975349f0d
Rajit Gohel <59544276+Rajit28@users.noreply.github.com>
2020-04-22 00:24:23 -0400

    Added PUT,PATCH,DELETE for Users

--- routes/users.js
+++ routes/users.js
@@ -57,4 +57,71 @@ router.post('/', async (req, res) => {
 	}
 });
 
+router.
+    put('/:id', async (req, res) => {
+        const updatedData = req.body;
+        if (!updatedData.firstName || !updatedData.lastName || !updatedData.email || !updatedData.city || !updatedData.state) {
+            res.status(400).json({ error: 'Please provide all the user information to update your registration' });
+            return;
+        }
+        try {
+            await usersData.getUserById(req.params.id);
+        } catch (e) {
+            res.status(404).json({ error: 'Registered User Not not found to Update the details' });
+            return;
+        }
+
+        try {
+            const updatedUser = await usersData.updateUser(req.params.id, updatedData.firstName, updatedData.lastName, updatedData.email, updatedData.city, updatedData.state);
+            res.json(updatedUser);
+        } catch (e) {
+            res.status(500).json({ error: e });
+        }
+    });
+
+router.patch('/:id', async (req, res) => {
+    const requestBody = req.body;
+    let updatedObject = {};
+    try {
+        const oldPost = await usersData.getUserById(req.params.id);
+        if (requestBody.firstName && requestBody.firstName !== oldPost.firstName) updatedObject.firstName = requestBody.firstName;
+        if (requestBody.lastName && requestBody.lastName !== oldPost.lastName) updatedObject.lastName = requestBody.lastName;
+        if (requestBody.email && requestBody.email !== oldPost.email) updatedObject.email = requestBody.email;
+        if (requestBody.city && requestBody.city !== oldPost.city)
+            updatedObject.city = requestBody.city;
+        if (requestBody.state && requestBody.state !== oldPost.state)
+            updatedObject.state = requestBody.state;
+    } catch (e) {
+        res.status(404).json({ error: 'User not found to modify the record' });
+        return;
+    }
+
+    try {
+        const updatedUser = await usersData.updateUser(req.params.id, requestBody.firstName, requestBody.lastName, requestBody.email, requestBody.city, requestBody.state);
+        res.json(updatedUser);
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    if (!req.params.id) {
+        res.status(400).json({ error: 'You must Supply id to delete' });
+        return;
+    }
+    try {
+        await usersData.getUserById(req.params.id);
+    } catch (e) {
+        res.status(404).json({ error: 'User not found' });
+        return;
+    }
+    try {
+        await usersData.removeUser(req.params.id);
+        res.sendStatus(200);
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
+
+
 module.exports = router;
