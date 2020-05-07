0f89a1cec935ebdc6875d1d81620d0381cd57e18
Rajit Gohel <59544276+Rajit28@users.noreply.github.com>
2020-05-07 19:39:43 -0400

    login

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 const issuesRoutes = require("./issues");
 const usersRoutes = require("./users");
 const express = require('express');
-const session = require('express-session');
-const router = express.Router();
-const bcrypt = require('bcryptjs');
+const outRoutes = require('./loginRoutes')
 
-const constructorMethod = (app) => {
-  app.use("/issues", issuesRoutes);
-  app.use("/users", usersRoutes);
-  app.use("/signup", (req, res) => {
-    res.render('grievances/signup');
-  });
+const constructorMethod = app => {
+  app.use("/issues", issuesRoutes)
+  app.use("/users", usersRoutes)
+  app.use("/", outRoutes)
+
+  // app.use("/signup", (req, res) => {
+  //   res.render('grievances/signup');
+  // });
+  // app.use("/login", (req, res) => {
+  //   res.render('grievances/login');
+  // });
+
   // app.post('/test', async (req, res) => {
   //   try{
   //       let data = req.body;
@@ -21,10 +25,8 @@ const constructorMethod = (app) => {
   //   res.sendStatus(400);
   // }
   // });
-  app.use("/", (req, res) => {
-    res.render('grievances/index');
-  });
-  
+
+
 };
 
 
