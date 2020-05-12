5aa7cae9bb7505c1d419bf8feb12d6f362074b16
Rajit Gohel <59544276+Rajit28@users.noreply.github.com>
2020-05-12 15:54:00 -0400

    xss added

--- routes/issues.js
+++ routes/issues.js
@@ -5,33 +5,34 @@ const issuesData = data.issues;
 const usersData = data.users;
 const { ObjectId } = require('mongodb');
 const file = require('../public/file.js')
+const xss = require('xss'); // xss checking
 
 router.get('/createIssue', async (req, res) => {
-    try {
-        if(req.session.user === undefined){
-            res.render('grievances/login', {message: "You must login first!"});
-        } else {
-            res.render('grievances/createIssue');
-        }
-    } catch (error) {
-        res.status(401).json({ error: "Page Not Found" });
-    }
+	try {
+		if (req.session.user === undefined) {
+			res.render('grievances/login', { message: "You must login first!" });
+		} else {
+			res.render('grievances/createIssue');
+		}
+	} catch (error) {
+		res.status(401).json({ error: "Page Not Found" });
+	}
 });
 
 router.get('/:id', async (req, res) => {
 	try {
-		if(req.session.user === undefined){
-            res.render('grievances/login', {message: "You must login first!"});
+		if (req.session.user === undefined) {
+			res.render('grievances/login', { message: "You must login first!" });
 		}
-		else{
+		else {
 			let issue = await issuesData.getIssueById(req.params.id);
 			const issueList = [issue];
-			if(issueList === null || issueList === undefined)
+			if (issueList === null || issueList === undefined)
 				res.status(404).json({ error: 'No issue found' })
 			else
-				res.render('grievances/viewIssue',{issueList:issueList});
+				res.render('grievances/viewIssue', { issueList: issueList });
 		}
-		
+
 	} catch (e) {
 		res.status(404).json({ error: 'issue not found' });
 	}
@@ -40,10 +41,10 @@ router.get('/:id', async (req, res) => {
 router.get('/user/:id', async (req, res) => {
 	try {
 		let issueList = await issuesData.getIssuesByUserId(req.params.id);
-		if(issueList === null || issueList === undefined)
+		if (issueList === null || issueList === undefined)
 			res.status(404).json({ error: 'No issue found' })
 		else
-			res.render('grievances/issue',{issueList:issueList});
+			res.render('grievances/issue', { issueList: issueList });
 	} catch (e) {
 		res.status(404).json({ error: 'issue not found' });
 	}
@@ -52,19 +53,20 @@ router.get('/user/:id', async (req, res) => {
 router.get('/', async (req, res) => {
 	try {
 		let issueList = await issuesData.getAllIssues();
-		res.render('grievances/issue',{issueList:issueList});
+		res.render('grievances/issue', { issueList: issueList });
 	} catch (e) {
 		res.sendStatus(400);
 	}
 });
 
 router.post('/comment/:id', async (req, res) => {
-    try {
-		if(req.session.user === undefined){
-            res.render('grievances/login', {message: "You must login first!"});
+	try {
+		if (req.session.user === undefined) {
+			res.render('grievances/login', { message: "You must login first!" });
 		}
-		else{
-			if (!req.body.content) {
+		else {
+
+			if (!xss(req.body.content)) { // xss added
 				console.log("No content typed");
 			} else {
 				await issuesData.addComment(req.session.user, req.body.content, req.params.id);
@@ -72,32 +74,32 @@ router.post('/comment/:id', async (req, res) => {
 
 			let issueid = req.params.id
 			const user = await usersData.getUserByEmail(req.session.user)
-			if(user.admin === false){
-				res.redirect('/issues/'+issueid);
+			if (user.admin === false) {
+				res.redirect('/issues/' + issueid);
 			}
-			else{
+			else {
 				res.redirect("/issues");
 			}
-			
-		} 
+
+		}
 	} catch (e) {
 		res.sendStatus(400);
 	}
 });
 
 router.post('/close/:id', async (req, res) => {
-    try {
-		if(req.session.user === undefined){
-            res.render('grievances/login', {message: "You must login as Admin first!"});
+	try {
+		if (req.session.user === undefined) {
+			res.render('grievances/login', { message: "You must login as Admin first!" });
 		}
-		else{
+		else {
 			const user = await usersData.getUserByEmail(req.session.user)
-			if(user.admin === true){
+			if (user.admin === true) {
 				await issuesData.closeIssue(req.params.id);
 				res.redirect("/issues");
 			}
-			else{
-				res.render('grievances/login', {message: "You must login as Admin first!"});
+			else {
+				res.render('grievances/login', { message: "You must login as Admin first!" });
 			}
 		}
 	} catch (e) {
@@ -106,18 +108,18 @@ router.post('/close/:id', async (req, res) => {
 });
 
 router.post('/open/:id', async (req, res) => {
-    try {
-		if(req.session.user === undefined){
-            res.render('grievances/login', {message: "You must login as Admin first!"});
+	try {
+		if (req.session.user === undefined) {
+			res.render('grievances/login', { message: "You must login as Admin first!" });
 		}
-		else{
+		else {
 			const user = await usersData.getUserByEmail(req.session.user)
-			if(user.admin === true){
-			await issuesData.openIssue(req.params.id);
-			res.redirect("/issues");
+			if (user.admin === true) {
+				await issuesData.openIssue(req.params.id);
+				res.redirect("/issues");
 			}
-			else{
-				res.render('grievances/login', {message: "You must login as Admin first!"});
+			else {
+				res.render('grievances/login', { message: "You must login as Admin first!" });
 			}
 		}
 	} catch (e) {
@@ -126,8 +128,8 @@ router.post('/open/:id', async (req, res) => {
 });
 
 router.post('/like/:id', async (req, res) => {
-    try {
-        await issuesData.likeIssue(req.params.id);
+	try {
+		await issuesData.likeIssue(req.params.id);
 		res.redirect("/issues");
 	} catch (e) {
 		res.sendStatus(400);
@@ -135,8 +137,8 @@ router.post('/like/:id', async (req, res) => {
 });
 
 router.post('/unlike/:id', async (req, res) => {
-    try {
-        await issuesData.unlikeIssue(req.params.id);
+	try {
+		await issuesData.unlikeIssue(req.params.id);
 		res.redirect("/issues");
 	} catch (e) {
 		res.sendStatus(400);
@@ -144,8 +146,8 @@ router.post('/unlike/:id', async (req, res) => {
 });
 
 router.post('/createIssue', async (req, res) => {
-	let issueInfo = req.body;
-	if(req.session.user === undefined){
+	let issueInfo = xss(req.body);
+	if (req.session.user === undefined) {
 		res.status(400).json({ error: 'You must provide user ID' });
 		return;
 	}
@@ -155,7 +157,7 @@ router.post('/createIssue', async (req, res) => {
 	if (!issueInfo) {
 		res.status(400).json({ error: 'You must provide data to create a user' });
 		return;
-    }
+	}
 	if (!issueInfo.name) {
 		res.status(400).json({ error: 'You must provide an issue name' });
 		return;
@@ -175,34 +177,34 @@ router.post('/createIssue', async (req, res) => {
 	if (!issueInfo.longitude) {
 		res.status(400).json({ error: 'Longitude information is not present' });
 		return;
-    }
-    if(!issueInfo.city){
-        res.status(400).json({ error: 'You must provide city' });
+	}
+	if (!issueInfo.city) {
+		res.status(400).json({ error: 'You must provide city' });
 		return;
-    }
+	}
 	if (!issueInfo.state) {
-			res.status(400).json({ error: 'You must provide state' });
-			return;
+		res.status(400).json({ error: 'You must provide state' });
+		return;
 	}
 
 	try {
 		const newIssue = await issuesData.addIssue(issueInfo.name, issueInfo.category,
-						issueInfo.date, issueInfo.latitude, issueInfo.longitude,
-						issueInfo.city, issueInfo.state, issueInfo.userID);
+			issueInfo.date, issueInfo.latitude, issueInfo.longitude,
+			issueInfo.city, issueInfo.state, issueInfo.userID);
 
-        //req.session.issue = newUser.email
+		//req.session.issue = newUser.email
 		//req.session.AuthCookie = req.sessionID;
-        let sessionInfo = req.session.user;
+		let sessionInfo = req.session.user;
 		const issueList = [newIssue];
-		res.render('grievances/viewIssue',{issueList:issueList,sessionInfo:sessionInfo});
+		res.render('grievances/viewIssue', { issueList: issueList, sessionInfo: sessionInfo });
 	} catch (e) {
 		res.sendStatus(400);
 	}
 });
 
 router.post('/download', async (req, res) => {
-    try {
-        file.downloadFile();
+	try {
+		file.downloadFile();
 		res.redirect("/issues");
 	} catch (e) {
 		res.sendStatus(400);
