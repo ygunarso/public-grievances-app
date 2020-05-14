52256c78ec810f3e50822b4cf6c371fc94e27976
Devila Bakrania <60561306+devilabakrania@users.noreply.github.com>
2020-05-13 22:44:37 -0400

    Refactoring of the code

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -1,17 +1,17 @@
 const MongoClient = require('mongodb').MongoClient;
-const settings = require('./settings');
-const mongoConfig = settings.mongoConfig;
+// const settings = require('./settings');
+// const mongoConfig = settings.mongoConfig;
 
 let _connection = undefined;
 let _db = undefined;
 
 module.exports = async () => {
 	if (!_connection) {
-		_connection = await MongoClient.connect(mongoConfig.serverUrl, {
+		_connection = await MongoClient.connect("mongodb://localhost:27017/", {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
 		});
-		_db = await _connection.db(mongoConfig.database);
+		_db = await _connection.db("Public_Grievances");
 	}
 
 	return _db;
